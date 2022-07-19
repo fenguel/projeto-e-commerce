7ef95742936c4323a0fa8ef3b0b37504424263d3
Felipe Rosa <106786186+fenguel@users.noreply.github.com>
2022-07-18 22:22:44 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,13 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 
-var currentNumber = 0;
-
-function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+function increment(id) {
+    let element = document.getElementById(id);
+    let counter = Number(element.textContent)+1;
+    element.innerHTML = counter;
 }
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
-
-
-var currentNumberWrapper2 = document.getElementById("currentNumber2");
-
-var currentNumber2 = 0;
-
-function increment2() {
-    currentNumber2 = currentNumber2 + 1;
-    currentNumberWrapper2.innerHTML = currentNumber2;
-}
-
-function decrement2() {
-    currentNumber2 = currentNumber2 - 1;
-    currentNumberWrapper2.innerHTML = currentNumber2;
+function decrement(id) {
+    let element = document.getElementById(id);
+    let counter = Number(element.textContent)-1;
+    element.innerHTML = counter;
 }
