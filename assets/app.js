11bbde5d7d32e6b6e72b292811b425705ff01880
karinemaiero <karine_gino@yahoo.com.br>
2022-07-20 01:11:05 -0300

    modificações de cores

--- assets/app.js
+++ assets/app.js
@@ -7,14 +7,8 @@ function increment(id) {
 }
 
 
-    function decrement(id) {
-            let element = document.getElementById(id);
-            let counter = Number(element.textContent)-1;
-            element.innerHTML = counter;
-            
-        }
-
-   
-    
-
-
+function decrement(id) {
+    let element = document.getElementById(id);
+    let counter = Number(element.textContent)-1;
+    element.innerHTML = counter;     
+}
