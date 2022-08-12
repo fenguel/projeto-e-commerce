fc580eb30ee9ccaec1d5da503bc9c6456faf37e1
fenguel <felipedelima@hotmail.com>
2022-08-12 12:26:30 -0300

    JavaScriptCarrinho

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,164 @@ function increment(id) {
     let element = document.getElementById(id);
     let counter = Number(element.textContent)+1;
     element.innerHTML = counter;
-}
-
+    }
+ 
+ 
 
 function decrement(id) {
     let element = document.getElementById(id);
     let counter = Number(element.textContent)-1;
-    element.innerHTML = counter;     
-}
+    if (counter >= 0) {
+        element.innerHTML = counter;
+    }
+    }
+
+    function incrementShop1(id) {
+        let element = document.getElementById(id);
+        let element2 = Number(document.getElementById('counterprod1').innerText);
+        let counter = Number(element.textContent)+element2;
+        element.innerHTML = counter;
+        document.getElementById('counterprod1').textContent = "0";
+    }
+        
+        
+
+    function incrementShop2(id) {
+        let element = document.getElementById(id);
+        let element2 = Number(document.getElementById('counterprod2').innerText);
+        let counter = Number(element.textContent)+element2;
+        element.innerHTML = counter;
+        document.getElementById('counterprod2').textContent = "0";
+        }
+
+    function incrementShop3(id) {
+        let element = document.getElementById(id);
+        let element2 = Number(document.getElementById('counterprod3').innerText);
+        let counter = Number(element.textContent)+element2;
+        element.innerHTML = counter;
+        document.getElementById('counterprod3').textContent = "0";
+        }    
+
+        function incrementShop4(id) {
+            let element = document.getElementById(id);
+            let element2 = Number(document.getElementById('counterprod4').innerText);
+            let counter = Number(element.textContent)+element2;
+            element.innerHTML = counter;
+            document.getElementById('counterprod4').textContent = "0";
+            }   
+
+            function incrementShop5(id) {
+                let element = document.getElementById(id);
+                let element2 = Number(document.getElementById('counterprod5').innerText);
+                let counter = Number(element.textContent)+element2;
+                element.innerHTML = counter;
+                document.getElementById('counterprod5').textContent = "0";
+                }   
+
+                function incrementShop6(id) {
+                    let element = document.getElementById(id);
+                    let element2 = Number(document.getElementById('counterprod6').innerText);
+                    let counter = Number(element.textContent)+element2;
+                    element.innerHTML = counter;
+                    document.getElementById('counterprod6').textContent = "0";
+                    }    
+
+                    function incrementShop7(id) {
+                        let element = document.getElementById(id);
+                        let element2 = Number(document.getElementById('counterprod7').innerText);
+                        let counter = Number(element.textContent)+element2;
+                        element.innerHTML = counter;
+                        document.getElementById('counterprod7').textContent = "0";
+                        }    
+
+                        function incrementShop8(id) {
+                            let element = document.getElementById(id);
+                            let element2 = Number(document.getElementById('counterprod8').innerText);
+                            let counter = Number(element.textContent)+element2;
+                            element.innerHTML = counter;
+                            document.getElementById('counterprod8').textContent = "0";
+                            }    
+
+                            function incrementShop9(id) {
+                                let element = document.getElementById(id);
+                                let element2 = Number(document.getElementById('counterprod9').innerText);
+                                let counter = Number(element.textContent)+element2;
+                                element.innerHTML = counter;
+                                document.getElementById('counterprod9').textContent = "0";
+                                }    
+
+                                function incrementShop10(id) {
+                                    let element = document.getElementById(id);
+                                    let element2 = Number(document.getElementById('counterprod10').innerText);
+                                    let counter = Number(element.textContent)+element2;
+                                    element.innerHTML = counter;
+                                    document.getElementById('counterprod10').textContent = "0";
+                                    }    
+
+
+                                    function incrementShop11(id) {
+                                        let element = document.getElementById(id);
+                                        let element2 = Number(document.getElementById('counterprod11').innerText);
+                                        let counter = Number(element.textContent)+element2;
+                                        element.innerHTML = counter;
+                                        document.getElementById('counterprod11').textContent = "0";
+                                        }    
+
+                                        function incrementShop12(id) {
+                                            let element = document.getElementById(id);
+                                            let element2 = Number(document.getElementById('counterprod12').innerText);
+                                            let counter = Number(element.textContent)+element2;
+                                            element.innerHTML = counter;
+                                            document.getElementById('counterprod12').textContent = "0";
+                                            }    
+
+                                            function incrementShop13(id) {
+                                                let element = document.getElementById(id);
+                                                let element2 = Number(document.getElementById('counterprod13').innerText);
+                                                let counter = Number(element.textContent)+element2;
+                                                element.innerHTML = counter;
+                                                document.getElementById('counterprod13').textContent = "0";
+                                                }    
+
+                                                function incrementShop14(id) {
+                                                    let element = document.getElementById(id);
+                                                    let element2 = Number(document.getElementById('counterprod14').innerText);
+                                                    let counter = Number(element.textContent)+element2;
+                                                    element.innerHTML = counter;
+                                                    document.getElementById('counterprod14').textContent = "0";
+                                                    }    
+
+
+                                                    function incrementShop15(id) {
+                                                        let element = document.getElementById(id);
+                                                        let element2 = Number(document.getElementById('counterprod15').innerText);
+                                                        let counter = Number(element.textContent)+element2;
+                                                        element.innerHTML = counter;
+                                                        document.getElementById('counterprod15').textContent = "0";
+                                                        }    
+
+
+                                                        function incrementShop16(id) {
+                                                            let element = document.getElementById(id);
+                                                            let element2 = Number(document.getElementById('counterprod16').innerText);
+                                                            let counter = Number(element.textContent)+element2;
+                                                            element.innerHTML = counter;
+                                                            document.getElementById('counterprod16').textContent = "0";
+                                                            }    
+
+                                                            function incrementShop17(id) {
+                                                                let element = document.getElementById(id);
+                                                                let element2 = Number(document.getElementById('counterprod17').innerText);
+                                                                let counter = Number(element.textContent)+element2;
+                                                                element.innerHTML = counter;
+                                                                document.getElementById('counterprod17').textContent = "0";
+                                                                }    
+
+
+                                                                function incrementShop18(id) {
+                                                                    let element = document.getElementById(id);
+                                                                    let element2 = Number(document.getElementById('counterprod18').innerText);
+                                                                    let counter = Number(element.textContent)+element2;
+                                                                    element.innerHTML = counter;
+                                                                    document.getElementById('counterprod18').textContent = "0";
+                                                                    }    
