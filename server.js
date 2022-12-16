511d9ed693e08152c6f2a1420063a52efd8fdae9
karinemaiero <karine_gino@yahoo.com.br>
2022-12-16 13:35:30 -0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 const PORT = process.env.PORT
-// 3000, 6000, 6060, 8080, 8088
+// 3030, 3333, 6666, 6060, 8080, 8000
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
